d8e2d9435f47cef8cd1fddd8dfaf5dcb8daecad0
Zach McMullen <zachmcmullen04@gmail.com>
2023-06-03 09:07:48 -0700

    [Fix] Mobile Menu

--- app/components/MobileMenu.tsx
+++ app/components/MobileMenu.tsx
@@ -9,7 +9,7 @@ export const MobileMenu = ({setMobileMenu} : any) => {
   const { data: session } = useSession()
 
   return (
-    <nav className="w-full h-screen fixed bottom-0 left-0 z-50 right-0 bg-black py-5 px-6">
+    <nav className="w-full flex flex-col fixed top-0 bottom-0 left-0 z-50 right-0 bg-black py-5 px-6">
       <div className="flex justify-between items-center">
         <div className="flex space-x-5 items-center text-lg md:text-2xl">
             <h1 className="font-bold">worksheet<span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500">ai</span></h1>
