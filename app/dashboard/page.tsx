9d92d09de40b240ce936057418fd1f1cc91d2dc3
Zach McMullen <zachmcmullen04@gmail.com>
2023-06-03 23:35:24 -0700

    [Fix] dashboard sorting

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
       const data = await res.json()
       const unorganizedWorksheets: Worksheet[] = data.worksheets
       const organizedWorksheets = unorganizedWorksheets.sort((a, b) => {
-        return (new Date(a.updatedAt)).getTime() - (new Date(b.updatedAt)).getTime()
+        return (new Date(b.updatedAt)).getTime() - (new Date(a.updatedAt)).getTime()
       })
       setWorksheets(organizedWorksheets)
       setFilteredWorksheetes(organizedWorksheets)
