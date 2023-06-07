798dc9b3cf75a50027924b34832f3723cd271f74
Zach McMullen <zachmcmullen04@gmail.com>
2023-06-07 14:48:02 -0700

    [Update] New Email address

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="mt-32 mx-6 md:mx-20 p-4 md:p-8 border-t border-neutral-500 text-neutral-500 flex flex-col space-y-2 md:flex-row md:space-y-0 md:items-center md:justify-between text-xs">
       <p className="flex items-center"><TbCopyright className="text-lg mr-1"/> worksheetai 2023. All rights reserved.</p>
-      <a href="mailto:zachmcmullen04@gmail.com?subject=worksheetai%20Feedback" className="flex items-center space-x-1 hover:opacity-80">
+      <a href="mailto:help@worksheetai.app?subject=worksheetai%20Feedback" className="flex items-center space-x-1 hover:opacity-80">
         <TbMail className="text-lg"/>
         <p>Email Feedback</p>
       </a>
